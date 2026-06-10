bada72955f844ffd44f6763954725ad0d5a69020
tranducco <tranducco27112004@gmail.com>
2026-06-10 11:17:36 +0700

    nop bai kiem tra 2

---
+++ EXAM2026_CA2/src/data.js
@@ -0,0 +1,44 @@
+const products = [
+    {
+        id: 1,
+        name: "iPhone 15",
+        category: "Điện thoại",
+        price: "20.000.000đ",
+        status: "Còn hàng"
+    },
+    { 
+        id: 2,
+        name: "Samsung Galaxy S23",
+        category: "Điện thoại", 
+        price: "18.000.000đ",
+        status: "Còn hàng"
+    },
+    {
+        id: 3,
+        name: "Xiaomi Mi 12",
+        category: "Điện thoại",
+        price: "15.000.000đ",
+        status: "Còn hàng"
+    },
+    {
+        id: 4,
+        name: "MacBook Pro 16 inch",
+        category: "Laptop",
+        price: "30.000.000đ",
+        status: "Còn hàng"
+    },
+    {
+        id: 5,
+        name: "Dell XPS 13",
+        category: "Laptop",
+        price: "25.000.000đ",
+        status: "Còn hàng"
+    },
+    {
+        id: 6,
+        name: "Tai nghe AirPods Pro",
+        category: "Phụ kiện",
+        price: "5.000.000đ",
+        status: "Còn hàng"
+    },
+]
